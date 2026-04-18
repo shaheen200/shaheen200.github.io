83144b90617f3b6896cd7d49fce15134ce878fe2
shaheen200 <mohamedbedosalah.2002@gmail.com>
2026-04-19 01:43:14 +0200

    razan new ui

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1390340982"
+    serviceWorkerVersion: "1313884144"
   }
 });
